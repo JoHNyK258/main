1646e3fd75fbb65a953735d0aaf3074db0d5c9bd
Evgeniy Myshinskikh <emishinskikh@gmail.com>
2020-11-25 20:50:30 +0600

    Шаблоны функций. Шаблонные функции c++. template typename. template class. Урок #41 ++. template typename. template class. Урок #41ease enter the commit message for your changes. Lines starting

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -3,40 +3,19 @@
 #include<ctime>
 
 using namespace std;
-
-double Sum(double a, double b, double c)
+template <class T1, class T2>
+void Sum(T1 a, T2 b)
 {
-	return a + b +c;
-}
-
-int Sum(int a, int b)
-{
-	return a + b;
-}
-
-
-
-
-double Sum(double a, double b)
-{
-	return a + b;
-}
-
-int Sum(int a, int b, int c)
-{
-	return a + b + c;
+	cout << a << endl;
+	cout << b << endl;
 
 }
-
-
-
-
 
 
 void main()
 {
-	cout << Sum(6, 5) << endl;
-	cout << Sum(6.3, 5.2) << endl;
-	cout << Sum(6, 5, 8) << endl;
-	cout << Sum(6.3, 5.5, 8.8) << endl;
+	setlocale(LC_ALL, "ru");
+
+Sum(5, 10); 
+
 }
