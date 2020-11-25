4deefca51e9c545526d348b80a3678077903c9c2
Evgeniy Myshinskikh <emishinskikh@gmail.com>
2020-11-25 21:06:29 +0600

    Сумма квадратов целых чисел

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -3,19 +3,21 @@
 #include<ctime>
 
 using namespace std;
-template <class T1, class T2>
-void Sum(T1 a, T2 b)
-{
-	cout << a << endl;
-	cout << b << endl;
-
-}
-
-
 void main()
 {
 	setlocale(LC_ALL, "ru");
 
-Sum(5, 10); 
+	int a=1, b=0, n;
+	cout << "¬ведите число: " << endl;
+	cin >> n;
+
+	while (a<=n)
+
+	{
+		b = b + a * a;
+		a++;
+	}
+
+	cout << "—умма квадратов: " << b << endl;
 
 }
